24111a9017f5f01a539a0b91c107c8a34025c7e2
nard00261 <120164934+nard00261@users.noreply.github.com>
2025-11-28 21:40:57 +0800

    Update voice.xml.php

--- public/voice.xml.php
+++ public/voice.xml.php
@@ -4,5 +4,5 @@ header('Content-Type: text/xml');
 $to = $_POST['To'] ?? '+639957802471'; // Default to a test number if not provided
 ?>
 <Response>
-    <Dial callerId="+13375105318"><?= htmlspecialchars($to) ?></Dial>
+    <Dial callerId="+17816509262"><?= htmlspecialchars($to) ?></Dial>
 </Response>
